a1e2d727a363a6e59b79ecdd4ea5ef0be78dd129
rimiti <dimitri.dobairro@dimsolution.com>
2018-02-19 16:41:39 +0100

    chore(classes/common/validation.js) validation class created to parse responses

---
+++ src/classes/common/validation.js
@@ -0,0 +1,20 @@
+export default class Validation {
+  /**
+   * @description Validate mandatory parameters.
+   * @param mandatoryParams
+   * @param params
+   * @return {Promise<any>}
+   */
+  static validateMandatoryParams(mandatoryParams, params = {}) {
+    return new Promise((resolve, reject) => {
+      const requestKeys = Object.keys(params);
+      const missingParams = mandatoryParams.filter((attribute) => requestKeys.indexOf(attribute) < 0);
+      if (missingParams.length === 1) {
+        return reject({error: {name: 'Oups...', message: `Parameter "${missingParams}" is missing.`}});
+      } else if (missingParams.length > 1) {
+        return reject({error: {name: 'Oups...', message: `Parameter(s) "${missingParams}" are missing.`}});
+      }
+      resolve(true);
+    });
+  }
+}
