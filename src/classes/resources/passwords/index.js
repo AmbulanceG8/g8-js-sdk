45589464e429ed17780248009ae3533bbf6e4fb8
rimiti <dimitri.dobairro@dimsolution.com>
2018-02-19 16:49:05 +0100

    chore(classes/resources/passwords/index.js) passwords class created

---
+++ src/classes/resources/passwords/index.js
@@ -0,0 +1,17 @@
+import Common from '../../common/common.js';
+import Validation from '../../common/validation';
+
+export default class Passwords extends Common {
+  /**
+   * @description Password forgotten.
+   * @param urlParams
+   * @param body
+   * @param headers
+   * @return {Promise}
+   */
+  create(urlParams, body, headers) {
+    const url = `${this.configuration.routes.passwords}`;
+    return Validation.validateMandatoryParams(['email'], body)
+      .then(() => super.postRequest(url, body, headers));
+  }
+}
