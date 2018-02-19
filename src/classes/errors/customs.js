fda80ccdaba0a78616ed06580f4d141db7ad7ee2
rimiti <dimitri.dobairro@dimsolution.com>
2018-02-19 16:43:16 +0100

    chore(classes/errors/customs.js) parse body error to beautiful errors

---
+++ src/classes/errors/customs.js
@@ -0,0 +1,20 @@
+import Common from './common';
+
+export default class Customs {
+  static getMessage(response) {
+    const message = response.data.message;
+    console.error(`[val/cus] - `, message);
+    if (message === 'BAD_CREDENTIALS_ERROR') {
+      return Common.formatError('Oups...', 'Email ou mot de passe incorrect.');
+    } else if (message === 'SCHEMA_VALIDATION_FAILED') {
+      return Common.formatError('Oups...', 'Un paramètre semble manquer.');
+    } else if (message === 'EMAIL_NOT_FOUND') {
+      return Common.formatError('Oups...', 'Email introuvable.');
+    } else if (message === 'PARAM_REQUIRED') {
+      return Common.formatError('Oups...', 'Un paramètre semble manquer.');
+    } else {
+      return Common.formatError(response.data.name, response.data.message);
+    }
+  }
+
+}
