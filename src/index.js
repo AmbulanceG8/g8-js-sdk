ce30e3d4d817659f4e7794947f2c7aca3a970601
rimiti <dimitri.dobairro@dimsolution.com>
2018-02-19 16:53:51 +0100

    feat(index.js) main file created

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import jsonOverride from 'json-override';
+import SDK from './entry_point';
+
+let configuration = {
+  authentication: '',
+  host: 'https://api.ambulance-g8.com',
+  routes: {
+    logins: '/logins',
+    passwords: '/passwords',
+    users: '/users',
+    usersDevicesPositions: '/users/{userId}/devices/{deviceId}/positions',
+  },
+};
+
+export default {
+  configure: (config) => configuration = jsonOverride(configuration, config),
+  create: () => new SDK(configuration),
+};
