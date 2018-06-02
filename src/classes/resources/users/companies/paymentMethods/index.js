5f8a095d9ee07f70765dccec9a22339926609e00
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-02 16:35:53 +0200

    feat(classes/resources/users/companies/paymentMethods/index.js) class created + create method

---
+++ src/classes/resources/users/companies/paymentMethods/index.js
@@ -0,0 +1,17 @@
+import Common from '../../../../common/common';
+import Validation from '../../../../common/validation';
+
+export default class UsersCompaniesPaymentMethods extends Common {
+  /**
+   * @description Create user's company payment method.
+   * @param urlParams
+   * @param body
+   * @param headers
+   * @return {Promise}
+   */
+  create(urlParams, body, headers) {
+    const url = Common.formatUrl(this.configuration.routes.usersCompaniesPaymentMethods, urlParams);
+    return Validation.validateMandatoryParams(['type', 'expiration_month', 'expiration_year', 'number', 'cvv2'], body)
+      .then(() => super.postRequest(url, body, headers));
+  }
+}
